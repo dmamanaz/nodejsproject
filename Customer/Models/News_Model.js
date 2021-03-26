56402e42eb505b09e249f6fdf3e847ef9e84b793
jetpotion <William.Zhang@stonybrook.edu>
2021-03-26 11:23:29 -0400

    Models Are Done

---
+++ Customer/Models/News_Model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+const NewsModel = new Schema({
+    News_title: {type:String},
+    News_description: {type:String},
+    News_url: {type:String},
+    News_urlToImage: {type:String},
+    News_publishedAt: {type:String},
+    News_insertTime: {type:Number}
+})
+module.exports = mongoose.model("News", ContactUsModel,"News_list")
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Project is seperated into 3 parts. 
-The Customer application and Admin application create web projects seperately from one another. While the deployer acts a file module to deploy said porjects
+The Customer application and Admin application create web projects seperately from one another. While the deployer acts a file module to deploy both projects
