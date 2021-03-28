1b77019d64bd1d7240d816c1536cbcdb6164dbeb
jetpotion <William.Zhang@stonybrook.edu>
2021-03-28 10:57:43 -0400

    Skeleton File Created

---
+++ Admin/Routes/AdminRouter.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const LocalStorage = require('node-localstorage').LocalStorage;
+const localStorage = new LocalStorage('./Scratch');
+
+const config = require('../config');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+const User = require('../Models/User_Model');
+const Newslist = require('../Models/News_Model')
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', './Views');
