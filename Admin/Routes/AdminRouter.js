cae331a5a835eeb3e0eb1b56127fb2cd753a4641
jetpotion <William.Zhang@stonybrook.edu>
2021-03-31 17:40:57 -0400

    FINAL VERSION OF PROJECT PLEASE FIX FOOTER, FOR BOTH ADMIN/CUSTOMER, I will put the javascript modal behavior soon

--- Admin/Routes/AdminRouter.js
+++ Admin/Routes/AdminRouter.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 const User = require('../Models/User_Model');
-const  Newslist = require('../Models/News_Model');
+const Newslist = require('../Models/News_Model');
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './Views');
@@ -18,7 +18,7 @@ const session = require('express-session');
 router.use(session({secret: 'edurekaSecret1', resave: false, saveUninitialized: true}));
 router.post('/login', (req, res) => {
 
-    User.findOne({ User_email: req.body.User_email }, (err, user) => {
+    User.findOne({ User_Email: req.body.User_Email }, (err, user) => {
       console.log("/login : user => ", user)
       if (err) return res.status(500).send('Error on the server.');
       let htmlMsg
@@ -26,7 +26,7 @@ router.post('/login', (req, res) => {
         htmlMsg = encodeURIComponent('Email not found, try again ...');
         res.redirect('/?invalid=' + htmlMsg);
       }else{
-        const passwordIsValid = bcrypt.compareSync(req.body.User_password, user.User_password);
+        const passwordIsValid = bcrypt.compareSync(req.body.User_Password, user.User_Password);
         if (!passwordIsValid) {
           return res.status(401).send({ auth: false, token: null });
         }
@@ -49,7 +49,7 @@ router.get('/newsForm', (req, res)=>{
     }
     jwt.verify(token, config.secret, (err, decoded)=>{
         if (err) { res.redirect('/') }
-        User.findById(decoded.id, { User_password: 0}, (err,user)=>{
+        User.findById(decoded.id, { User_Password: 0}, (err,user)=>{
             if (err) {res.redirect('/')}
             if (!user) {res.redirect('/')} 
             console.log("/newsForm : user ==> ", user)   
@@ -69,7 +69,7 @@ router.get('/getNews', (req, res)=>{
     }
     jwt.verify(token, config.secret, (err, decoded)=>{
         if (err) { res.redirect('/') }
-        User.findById(decoded.id, { User_password: 0}, (err,user)=>{
+        User.findById(decoded.id, { User_Password: 0}, (err,user)=>{
             if (err) {res.redirect('/')}
             if (!user) {res.redirect('/')} 
             console.log("/newsForm : user ==> ", user)   
@@ -167,15 +167,15 @@ router.get('/logout', (req,res) => {
 })
 router.post('/register', (req,res) => {
     console.log("/register : req.body ==> ", req.body)
-    User.findOne({ User_email: req.body.User_email }, (err, user) => {
+    User.findOne({User_Email: req.body.User_Email }, (err, user) => {
       if (err) return res.status(500).send('Error on the server.');
       let htmlMsg
       if(!user){ //add new user
-        const hashedPasword = bcrypt.hashSync(req.body.User_password, 8);
+        const hashedPasword = bcrypt.hashSync(req.body.User_Password, 8);
         User.create({
-            User_name: req.body.User_name,
-            User_email: req.body.User_email,
-            User_password: hashedPasword,
+            User_Name: req.body.User_Name,
+            User_Email: req.body.User_Email,
+            User_Password: hashedPasword
         }, (err, user) => {
             if(err) return res.status(500).send('There was a problem registering user')
             htmlMsg = encodeURIComponent('Registered OK !');
@@ -186,5 +186,12 @@ router.post('/register', (req,res) => {
         res.redirect('/?msg=' + htmlMsg);
       }
     })     
+})
+
+router.get('/register',(req,res) =>
+{
+        
+        res.render("signup.ejs");
+        
 })
 module.exports = {router,User,Newslist};
